1f92ab87359d9ee145c5cb9dedf80c6304dfc616
ProfessionalTiger <aamirsaleem.sqa@gmail.com>
2026-04-19 00:48:13 +0500

    Fixing the failed test cases

--- cypress/e2e/Youtube_TestCases.cy.js
+++ cypress/e2e/Youtube_TestCases.cy.js
@@ -9,7 +9,7 @@ describe("Youtube Testing Suite", () => {
     it("TC_001 Search cypress courses and play that ", () => {
 
         cy.get("input[name='search_query']").type("cypress");
-        cy.get(".ytSuggestionComponentLeftContainer:first()").click();
+        cy.get(".ytSuggestionComponentLeftContainer:first()").should('be.visible').click();
         cy.get("h3[title='Cypress - JavaScript End to End Testing(2022 Series)']>a").should('be.visible').click()
         cy.title().should('eql', "Part 1: Cypress E2E Web Automation | Introduction | 2022 Series - YouTube")
 
@@ -17,9 +17,9 @@ describe("Youtube Testing Suite", () => {
 
     it("TC_002 Count the number of thumbnails loaded ", () => {
 
-            cy.get(".style-scope ytd-rich-item-renderer").should('have.length', 47)
-
-        })
+        cy.get(".style-scope ytd-rich-item-renderer").should('be.visible')
+            .and('have.length', 47)
+    })
 
     it("TC_003 Load Shorts and validate that video played", () => {
 
